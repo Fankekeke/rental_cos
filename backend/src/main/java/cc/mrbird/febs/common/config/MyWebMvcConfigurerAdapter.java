9b003142039af6cef98816bf26f6ecbeb1e17d0f
悲伤的橘子树 <1258846421@qq.com>
2022-10-19 22:58:39 +0800

    fix -今天打了守望先锋

--- backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
+++ backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
@@ -9,7 +9,7 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:E:/Project/房屋租赁系统/rental_cos/file/");
+        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:E:/Project/房屋租赁系统/file/");
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
---
+++ frontend/src/views/manage/community/CommunityEdit.vue
@@ -0,0 +1,209 @@
+<template>
+  <a-modal v-model="show" title="修改小区" @cancel="onClose" :width="800">
+    <template slot="footer">
+      <a-button key="back" @click="onClose">
+        取消
+      </a-button>
+      <a-button key="submit" type="primary" :loading="loading" @click="handleSubmit">
+        修改
+      </a-button>
+    </template>
+    <a-form :form="form" layout="vertical">
+      <a-row :gutter="20">
+        <a-col :span="12">
+          <a-form-item label='小区标题' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'title',
+            { rules: [{ required: true, message: '请输入名称!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label='上传人' v-bind="formItemLayout">
+            <a-input v-decorator="[
+            'publisher',
+            { rules: [{ required: true, message: '请输入上传人!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label='小区类型' v-bind="formItemLayout">
+            <a-select v-decorator="[
+              'type',
+              { rules: [{ required: true, message: '请输入小区类型!' }] }
+              ]">
+              <a-select-option value="1">画报</a-select-option>
+              <a-select-option value="2">导购</a-select-option>
+              <a-select-option value="3">新盘发布</a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label='小区状态' v-bind="formItemLayout">
+            <a-select v-decorator="[
+              'rackUp',
+              { rules: [{ required: true, message: '请输入小区状态!' }] }
+              ]">
+              <a-select-option value="0">下架</a-select-option>
+              <a-select-option value="1">已发布</a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-col>
+        <a-col :span="24">
+          <a-form-item label='小区内容' v-bind="formItemLayout">
+            <a-textarea :rows="6" v-decorator="[
+            'content',
+             { rules: [{ required: true, message: '请输入名称!' }] }
+            ]"/>
+          </a-form-item>
+        </a-col>
+        <a-col :span="24">
+          <a-form-item label='图册' v-bind="formItemLayout">
+            <a-upload
+              name="avatar"
+              action="http://127.0.0.1:9527/file/fileUpload/"
+              list-type="picture-card"
+              :file-list="fileList"
+              @preview="handlePreview"
+              @change="picHandleChange"
+            >
+              <div v-if="fileList.length < 8">
+                <a-icon type="plus" />
+                <div class="ant-upload-text">
+                  Upload
+                </div>
+              </div>
+            </a-upload>
+            <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
+              <img alt="example" style="width: 100%" :src="previewImage" />
+            </a-modal>
+          </a-form-item>
+        </a-col>
+      </a-row>
+    </a-form>
+  </a-modal>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+function getBase64 (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+}
+const formItemLayout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 }
+}
+export default {
+  name: 'communityEdit',
+  props: {
+    communityEditVisiable: {
+      default: false
+    }
+  },
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    }),
+    show: {
+      get: function () {
+        return this.communityEditVisiable
+      },
+      set: function () {
+      }
+    }
+  },
+  data () {
+    return {
+      rowId: null,
+      formItemLayout,
+      form: this.$form.createForm(this),
+      loading: false,
+      fileList: [],
+      previewVisible: false,
+      previewImage: ''
+    }
+  },
+  methods: {
+    handleCancel () {
+      this.previewVisible = false
+    },
+    async handlePreview (file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj)
+      }
+      this.previewImage = file.url || file.preview
+      this.previewVisible = true
+    },
+    picHandleChange ({ fileList }) {
+      this.fileList = fileList
+    },
+    imagesInit (images) {
+      if (images !== null && images !== '') {
+        let imageList = []
+        images.split(',').forEach((image, index) => {
+          imageList.push({uid: index, name: image, status: 'done', url: 'http://127.0.0.1:9527/imagesWeb/' + image})
+        })
+        this.fileList = imageList
+      }
+    },
+    setFormValues ({...community}) {
+      this.rowId = community.id
+      let fields = ['title', 'content', 'publisher', 'rackUp', 'type']
+      let obj = {}
+      Object.keys(community).forEach((key) => {
+        if (key === 'images') {
+          this.fileList = []
+          this.imagesInit(community['images'])
+        }
+        if (key === 'rackUp' || key === 'type') {
+          community[key] = community[key].toString()
+        }
+        if (fields.indexOf(key) !== -1) {
+          this.form.getFieldDecorator(key)
+          obj[key] = community[key]
+        }
+      })
+      this.form.setFieldsValue(obj)
+    },
+    reset () {
+      this.loading = false
+      this.form.resetFields()
+    },
+    onClose () {
+      this.reset()
+      this.$emit('close')
+    },
+    handleSubmit () {
+      // 获取图片List
+      let images = []
+      this.fileList.forEach(image => {
+        images.push(image.name)
+      })
+      this.form.validateFields((err, values) => {
+        values.id = this.rowId
+        values.images = images.length > 0 ? images.join(',') : null
+        if (!err) {
+          this.loading = true
+          this.$put('/cos/community-info', {
+            ...values
+          }).then((r) => {
+            this.reset()
+            this.$emit('success')
+          }).catch(() => {
+            this.loading = false
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
