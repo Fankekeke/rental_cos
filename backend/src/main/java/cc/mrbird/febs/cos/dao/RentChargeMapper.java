ca3a0ca35d01bbc9ef74e87c6c836223c75a94de
悲伤的橘子树 <1258846421@qq.com>
2022-10-26 22:46:40 +0800

    add -添加负责人模块

---
+++ backend/src/main/java/cc/mrbird/febs/cos/dao/RentChargeMapper.java
@@ -0,0 +1,11 @@
+package cc.mrbird.febs.cos.dao;
+
+import cc.mrbird.febs.cos.entity.RentCharge;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author FanK
+ */
+public interface RentChargeMapper extends BaseMapper<RentCharge> {
+
+}
---
+++ backend/src/main/java/cc/mrbird/febs/cos/service/impl/RentChargeServiceImpl.java
@@ -0,0 +1,15 @@
+package cc.mrbird.febs.cos.service.impl;
+
+import cc.mrbird.febs.cos.entity.RentCharge;
+import cc.mrbird.febs.cos.dao.RentChargeMapper;
+import cc.mrbird.febs.cos.service.IRentChargeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author FanK
+ */
+@Service
+public class RentChargeServiceImpl extends ServiceImpl<RentChargeMapper, RentCharge> implements IRentChargeService {
+
+}
