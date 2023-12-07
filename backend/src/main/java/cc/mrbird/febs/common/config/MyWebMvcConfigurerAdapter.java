8bf7993abecf12cb863ef33da3d9a61cc4786c65
Fankekeke <1258846421@qq.com>
2023-12-07 22:50:26 +0800

    fix -用户端管理

--- backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
+++ backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
@@ -9,7 +9,7 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:E:/Project/房屋租赁系统/file/");
+        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:G:/Project/房屋租赁系统/file/");
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
--- web/src/views/rent/Rent.vue
+++ web/src/views/rent/Rent.vue
@@ -2,13 +2,10 @@
   <div :class="[multipage === true ? 'multi-page':'single-page', 'not-menu-page', 'home-page']">
     <a-card :bordered="false" class="card-area" style="width: 1200px;margin: 15px auto 0;">
       <div style="height: 500px;">
-        <div style="height: 350px;background-image: url(../../../static/img/living-room-1853203_1920.jpg);padding: 50px">
-          <div style="font-size: 35px;font-weight: 500;color: white;font-family: SimHei">安居客 全房源</div>
-          <div style="font-size: 22px;font-weight: 500;color: white;font-family: SimHei">让您找房更轻松</div>
-          <div style="height: 250px;margin-top: 100px">
+        <div style="height: 350px;background-image: url(../../../static/img/heart-3097561_1280.png);padding: 50px">
+          <div style="height: 250px;margin-top: 150px">
             <a-card :bordered="false" hoverable style="height: 100%;box-shadow: 3px 3px 3px rgba(0, 0, 0, .2);color:#fff">
               <a-row style="color: #404040;margin-left: 60px;margin-top: 20px">
-                <a-col :span="24"><b style="font-size: 13px;font-family: SimHei">位置：</b>全部</a-col>
                 <a-col :span="24" style="font-size: 15px;font-family: SimHei;margin-top: 15px"><b style="font-size: 13px;">租金：</b>
                   <a style="margin-right: 15px">1000以下</a>
                   <a style="margin-right: 15px">1000~1500</a>
@@ -34,12 +31,35 @@
                   <a style="margin-right: 15px">整租</a>
                   <a style="margin-right: 15px">合租</a>
                 </a-col>
-                <a-col :span="24" style="font-size: 15px;font-family: SimHei;margin-top: 15px"><b style="font-size: 13px;">更多筛选：</b>
-                </a-col>
               </a-row>
             </a-card>
           </div>
         </div>
+      </div>
+      <div style="padding: 50px;padding-top: 0;margin: 15px auto 0;">
+        <a-list item-layout="vertical" size="large" :data-source="rentList">
+          <a-list-item slot="renderItem" key="item.title" slot-scope="item, index">
+            <img
+              slot="extra"
+              width="272"
+              alt="logo"
+              :src="'http://127.0.0.1:9527/imagesWeb/' + item.roomPictures.split(',')[0]"
+            />
+            <a-list-item-meta :description="item.communityName">
+              <a slot="title">{{ item.title }}</a>
+            </a-list-item-meta>
+            <div style="font-size: 12px;font-family: SimHei;">
+              {{ item.rentRemark }}
+            </div>
+            <br/>
+            <div style="font-size: 12px;font-family: SimHei;">
+              租金：<span style="color: red">{{ item.rentPrice }} 元</span>
+              <a-tag v-if="item.rentType == 1">整租</a-tag>
+              <a-tag v-if="item.rentType == 2">合租</a-tag>
+              <a-tag>{{ item.roomNumber }}室</a-tag>
+            </div>
+          </a-list-item>
+        </a-list>
       </div>
     </a-card>
   </div>
@@ -51,6 +71,7 @@ export default {
   name: 'Rent',
   data () {
     return {
+      rentList: []
     }
   },
   computed: {
@@ -58,6 +79,16 @@ export default {
       multipage: state => state.setting.multipage,
       user: state => state.account.user
     })
+  },
+  mounted () {
+    this.getRentList()
+  },
+  methods: {
+    getRentList () {
+      this.$get(`/cos/rent-info/page`, {flag: '1'}).then((r) => {
+        this.rentList = r.data.data.records
+      })
+    }
   }
 }
 </script>
@@ -65,5 +96,11 @@ export default {
 <style scoped>
   >>> .ant-card-body {
     padding: 0;
+  }
+
+  >>> .ant-list-item-meta-title {
+    font-weight: 700;
+    font-size: 14px;
+    font-family: SimHei;
   }
 </style>
